3e5fda84c41398576445ecedd3b9b51afd464858
Noam Glassman <noamg1911@gmail.com>
2024-12-27 20:58:32 +0200

    added docs

--- heap_sorter/heap_sorter.hpp
+++ heap_sorter/heap_sorter.hpp
@@ -5,10 +5,26 @@
 
 #define ROOT_INDEX (0)
 
+
+/*
+A class that represents an implementation of the heap_sort algorithm. 
+
+@template param MaxHeapArraySize: The maximal size of the heap array (maximal because in heap sort you decrement
+the size of the heap).
+@member heap_array: an Array that holds the values given in the heap sort function.
+@member current_heap_size: The current size of the heap (not the array, because in heap sort the heap size is decremented).
+@member num_of_children: The number of children for each parent in the heap.
+@member max_parent_index: The index in the array of the last parent in the heap (parent -> has children).
+@member num_comparisons: The number of comparisons that were made on heap_array during the function runs.
+@member num_copies: The number of copies that were made on heap_array during the function runs.
+*/
 template<size_t MaxHeapArraySize>
 class HeapSorter
 {
 public:
+    /*
+    Constructor. Sets the relevant members to their initial values, and zeroes out the heap_array member.
+    */
     HeapSorter(size_t num_of_children) : _num_of_children(num_of_children)
     {
         for (size_t index = 0; index < MaxHeapArraySize; index++)
@@ -18,15 +34,16 @@ public:
         this->initialize_non_array_members();
     }
 
+    /*
+    Central function of the class. Implements the heap sort algorithm by taking an array, reorganizing it to make a
+    heap, and then sorts that heap.
+
+    @param num_of_children: The number of children each parent in the created heap will have
+    @param unordered_array: The array to be sorted (the values of the array are copied here so the array itself isn't sorted).
+    */
     void heap_sort(size_t num_of_children, size_t (&unordered_array)[MaxHeapArraySize])
     {
-        this->initialize_non_array_members();
-        this->set_num_of_children(num_of_children);   
-        for (size_t index = 0; index < MaxHeapArraySize; index++)
-        {
-            this->_heap_array[index] = unordered_array[index];
-        }
-
+        this->initialize_data_for_heap_sort(num_of_children, unordered_array);
         this->build_max_heap();
 
         for (int index = MaxHeapArraySize - 1; index > 0; index--)
@@ -38,6 +55,12 @@ public:
         }
     }
 
+    /*
+    Implementation of the max heapify function. "Sifts" down a given index in a heap array, recursively calling the 
+    function until the index's heap subtree is "legal".
+    
+    @param index: The index to be "sifted" down to make the index's heap subtree legal.
+    */
     void max_heapify(size_t index)
     {
         size_t largest_child_index = this->get_index_of_largest_child(index);
@@ -50,20 +73,20 @@ public:
         this->_num_comparisons++;
     }
 
+    /*
+    Turns the class's array into a legal max heap. Uses max heapify calls on the array's parent indexes.
+    */
     void build_max_heap()
     {
         for (int current_parent_index = this->_max_parent_index; current_parent_index >= 0; current_parent_index--)
         {
-            max_heapify(current_parent_index);
+            this->max_heapify(current_parent_index);
         }
     }
 
-    void set_num_of_children(size_t new_num_of_children)
-    {
-        this->_num_of_children = new_num_of_children;
-        this->update_max_parent_index();
-    }
-
+    /*
+    Prints the values of the elements in the class's array. Used mainly for debugging.
+    */
     void print_heap()
     {
         for (size_t index = 0; index < MaxHeapArraySize; index++)
@@ -73,17 +96,28 @@ public:
         printf("\n");
     }
 
+    /*
+    Returns the number of comparisons that were currently made on the objects of the class's array.
+    */
     size_t get_comparisons()
     {
         return this->_num_comparisons;
     }
 
+    /*
+    Returns the number of value copies that were currently made on the objects of the class's array.
+    */
     size_t get_copies()
     {
         return this->_num_copies;
     }
 
 private:
+    /*
+    Swaps the values of two elements in the class's heap array
+
+    @param first_index, second_index: The indexes of the elements to swap.
+    */
     void swap_values(size_t first_index, size_t second_index)
     {
         if (first_index > this->_current_heap_size - 1 || second_index > this->_current_heap_size - 1)
@@ -98,6 +132,25 @@ private:
         this->_num_copies += 2;
     }
 
+    /*
+    Sets the number of children each parent in the heap will have, and calculates the maximal index of a 
+    parent element in the array (all indexes after it are just children).
+
+    @param new_num_of_children: The new number of children each parent will have.
+    */
+    void set_num_of_children(size_t new_num_of_children)
+    {
+        this->_num_of_children = new_num_of_children;
+        this->update_max_parent_index();
+    }
+
+    /*
+    Gets the array index of the leftmost child of a given heap parent element
+
+    @param parent_index: The array index of the parent element.
+    
+    @return The index of the leftmost child.
+    */
     size_t get_left_child_index(size_t parent_index)
     {
         if (parent_index > this->_max_parent_index)
@@ -108,6 +161,13 @@ private:
         return parent_index * this->_num_of_children + 1;
     }
 
+    /*
+    Gets the array index of the parent of a given heap child element
+
+    @param child_index: The array index of the child element.
+    
+    @return The index of the parent.
+    */
     size_t get_parent_index(size_t child_index)
     {
         if (child_index == ROOT_INDEX)
@@ -118,6 +178,13 @@ private:
         return (child_index - 1) / this->_num_of_children;
     }
 
+    /*
+    Calculates and gets the index of a given parent element's heap child element with the largest value.
+
+    @param parent_index: The array index of the parent element.
+    
+    @return The index of the child with the largest value (not index value, array value).
+    */
     size_t get_index_of_largest_child(size_t parent_index)
     {
         if (parent_index > this->_max_parent_index || this->_current_heap_size <= 1)
@@ -140,11 +207,30 @@ private:
         return largest_child_index;
     }
 
+    /*
+    Calculates the biggest array index of a parent element in the heap array (all other elements after are non-parent elements)
+    */
     void update_max_parent_index()
     {
         this->_max_parent_index = this->get_parent_index(this->_current_heap_size - 1);
     }
 
+    /*
+    Initializes member data for the heap_sort function.
+    */
+    void initialize_data_for_heap_sort(size_t num_of_children, size_t (&unordered_array)[MaxHeapArraySize])
+    {
+        this->initialize_non_array_members();
+        this->set_num_of_children(num_of_children);   
+        for (size_t index = 0; index < MaxHeapArraySize; index++)
+        {
+            this->_heap_array[index] = unordered_array[index];
+        }
+    }
+
+    /*
+    Initializes some of the members to their default value (used in constructor and before running heap sort).
+    */
     void initialize_non_array_members()
     {
         this->_num_comparisons = 0;
--- main.cpp
+++ main.cpp
@@ -1,7 +1,4 @@
 #include <time.h>
-#include <math.h>
-#include <array>
-#include <tuple>
 #include "heap_sorter/heap_sorter.hpp"
 
 #define MIN_VAL (0)
@@ -12,63 +9,70 @@
 #define MIN_NUM_CHILDREN (2)
 #define MAX_NUM_CHILDREN (5)
 
+/*
+Puts random positive (ranged) integers in a given array.
 
-
-template<unsigned int ArraySize>
-void random_number_array_generator(size_t (&empty_array)[ArraySize], int min_value, int max_value)
+@template param ArraySize: The size of the array of numbers.
+@param o_empty_array: A (so-called) empty array that will be filled with random integers.
+@param min_value: The lower bound of the range of random integers.
+@param max_value: The upper bound of the range of random integers.
+*/
+template<size_t ArraySize>
+void random_number_array_generator(size_t (&o_empty_array)[ArraySize], int min_value, int max_value)
 {   
     size_t i = 0;
     int range = max_value - min_value + 1;
 
     for (i = 0; i < ArraySize; i++)
     {
-        empty_array[i] = size_t(min_value + (rand() % range));
+        o_empty_array[i] = size_t(min_value + (rand() % range));
     }
+}
+
+/*
+Prints out the number of comparisons and swaps made on a given array using the heap_sort algorithm with different
+numbers of heap children per parent.
+
+@template param HeapSize: The size of the array/heap.
+@param heap_sorter: The HeapSorter objet that implements the heap_sort algorithm.
+@param unordered_array: The array to be sorted using heap_sort.
+@param array_char: A character that represents the array (according to maman instructions).
+*/
+template<size_t HeapSize>
+void output_results_for_heapsort(HeapSorter<HeapSize> heap_sorter, size_t (&unordered_array)[HeapSize], char array_char)
+{
+    size_t num_of_children = MIN_NUM_CHILDREN;
+
+    printf("For Series %c:\n==============\n", array_char);
+
+    for (num_of_children = MIN_NUM_CHILDREN; num_of_children < MAX_NUM_CHILDREN + 1; num_of_children++)
+    {
+        heap_sorter.heap_sort(num_of_children, unordered_array);
+        printf("for d = %zu:\n", num_of_children);
+        printf("comparisons: %zu, copies: %zu\n", heap_sorter.get_comparisons(), heap_sorter.get_copies());
+    }
+
+    printf("===============\n");
+    printf("\n");
 }
 
 
 int main()
 {
-    srand(unsigned(time(0)));
     size_t series_A[A_SIZE] = { 0 }; 
     size_t series_B[B_SIZE] = { 0 }; 
     size_t series_C[C_SIZE] = { 0 }; 
 
+    srand(unsigned(time(0)));
     random_number_array_generator(series_A, MIN_VAL, MAX_VAL);
     random_number_array_generator(series_B, MIN_VAL, MAX_VAL);
     random_number_array_generator(series_C, MIN_VAL, MAX_VAL);
 
-    HeapSorter<A_SIZE> heap_sorter_A = HeapSorter<A_SIZE>(2);
-    HeapSorter<B_SIZE> heap_sorter_B = HeapSorter<B_SIZE>(2);
-    HeapSorter<C_SIZE> heap_sorter_C = HeapSorter<C_SIZE>(2);
+    HeapSorter<A_SIZE> heap_sorter_A = HeapSorter<A_SIZE>(MIN_NUM_CHILDREN);
+    HeapSorter<B_SIZE> heap_sorter_B = HeapSorter<B_SIZE>(MIN_NUM_CHILDREN);
+    HeapSorter<C_SIZE> heap_sorter_C = HeapSorter<C_SIZE>(MIN_NUM_CHILDREN);
 
-
-    // const auto matching_heap_sorters_series_letters = std::array{std::make_tuple(std::ref(heap_sorter_A), series_A, 'A'), std::make_tuple(std::ref(heap_sorter_B), series_B, 'B'), std::make_tuple(std::ref(heap_sorter_C), series_C, 'C'),};
-    size_t num_of_children;
-    char series_char = 'A';
-
-    printf("For Series %c\n", series_char);
-    printf("===============\n");
-    for (num_of_children = MIN_NUM_CHILDREN; num_of_children < MAX_NUM_CHILDREN + 1; num_of_children++)
-    {
-        heap_sorter_C.heap_sort(num_of_children, series_C);
-        printf("for d = %zu:\n", num_of_children);
-        printf("comparisons: %zu, copies: %zu\n", heap_sorter_C.get_comparisons(), heap_sorter_C.get_copies());
-    }
-    printf("===============\n");
-
-
-    // for (const auto& [heap_sorter, series, letter] : matching_heap_sorters_series_letters)
-    // {
-    //     printf("For Series %c\n", letter);
-    //     printf("===============\n");
-    //     for (num_of_children = MIN_NUM_CHILDREN; num_of_children < MAX_NUM_CHILDREN; num_of_children++)
-    //     {
-    //         heap_sorter.heap_sort(num_of_children, series);
-    //         printf("for d = %zu:\n", num_of_children);
-    //         printf("comparisons: %zu, copies: %zu\n", heap_sorter.get_comparisons(), heap_sorter.get_copies());
-    //     }
-    //     printf("===============\n");
-    // }
-    printf("\n");
+    output_results_for_heapsort(heap_sorter_A, series_A, 'A');
+    output_results_for_heapsort(heap_sorter_B, series_B, 'B');
+    output_results_for_heapsort(heap_sorter_C, series_C, 'C');
 }
